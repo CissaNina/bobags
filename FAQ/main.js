030143f400bb84d100eaeadf9baa202364976d0f
Alice Cissa Hpito <alicegpt2010@gmail.com>
2020-02-19 20:34:45 -0300

    criando faqs com transição...

---
+++ FAQ/main.js
@@ -0,0 +1,21 @@
+let text = document.querySelector('.item_list--text')
+
+function showInfo(){
+    if (text.style.display === ''){
+        text.style.display = 'flex'
+        text.style.height = '10vh'
+        text.style.opacity = '1'
+        // text.style.transition = 'height 1s'
+        text.style.transition = 'opacity 1s'
+    }
+    else {
+        text.style.display = ''
+        text.style.height = '0vh'
+        // text.style.transition = 'height 1s'
+        text.style.transition = 'opacity 0s'
+        text.style.opacity = '0'
+        
+    }
+}
+
+console.log(text.style)
