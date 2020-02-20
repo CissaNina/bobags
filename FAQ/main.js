89b36e0a2bc8c185ccfaa9af6f788a4931a4edd4
Alice Cissa Hpito <alicegpt2010@gmail.com>
2020-02-20 16:55:08 -0300

    Estilizando css e colocando funcionalidades...

--- FAQ/main.js
+++ FAQ/main.js
@@ -3,7 +3,7 @@ let text = document.querySelector('.item_list--text')
 function showInfo(){
     if (text.style.display === ''){
         text.style.display = 'flex'
-        text.style.height = '10vh'
+        text.style.height = '20vh'
         text.style.opacity = '1'
         // text.style.transition = 'height 1s'
         text.style.transition = 'opacity 1s'
@@ -17,5 +17,3 @@ function showInfo(){
         
     }
 }
-
-console.log(text.style)
